d466b11af5f63e7193cd192ed7c2ad3d94c53d91
thays <thayscasado@hotmail.com>
2021-10-09 23:27:05 -0700

    110. Using the useEffect() Hook (LocalStorage)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // check if user is logged in once
+  useEffect(() => {
+    const storedUserLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedIn === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // save the user status on the local storage
+    localStorage.setItem("isLoggedIn", "1"); // true
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    // remove the user status on the local storage
+    // localStorage.setItem("isLoggedIn", "0"); // false
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
